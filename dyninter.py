4666392417a76e8f94e0aa27565fb4f67fd421be
OURO-BANG'NA Ridoine <73545399+Ridoineel@users.noreply.github.com>
2021-08-06 17:40:32 +0000

    add the main file

---
+++ dyninter.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+# coding: utf-8;
+
+import argparse
+
+def parse_object():
+    """ Return command line arguments object """
+    
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-f', '--file', help="python file")
+
+    return argparser.parse_args()
+
+arg_datas = parse_object()
+
+if not arg_datas.file: 
+    exit()
+
+filepath = arg_datas.file
+last_content = ""
+
+while True:
+    _file = open(filepath, "r")
+    
+    # the file code (content)
+    content =  ''.join(_file.readlines())
+
+    if content != last_content:
+        print("<================= Start Execution ===================>\n")
+        exec(content)
+        print("\n<================= End Execution ===================>\n")
+        
+        last_content = content
+    
+    #end, close the file
+    _file.close()
